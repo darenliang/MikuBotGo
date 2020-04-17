2ea7df9e14f48994282ebe612101d81e92fc79da
Daren Liang <daren@darenliang.com>
2020-04-16 22:04:28 -0400

    Update

--- cmd/leaderboard.go
+++ cmd/leaderboard.go
@@ -12,7 +12,7 @@ import (
 func Leaderboard(ctx *exrouter.Context) {
 	highScores := framework.GetHighscores()
 
-	leaderboard := "```\nRank | Score | Count | User\n"
+	leaderboard := "```\nRank | Score | Invokes | User\n"
 
 	for idx, val := range highScores {
 		if idx == 20 {
@@ -20,7 +20,7 @@ func Leaderboard(ctx *exrouter.Context) {
 		}
 		leaderboard += fmt.Sprintf("#%-4d|", idx+1)
 		leaderboard += fmt.Sprintf(" %-6d|", val.MusicScore)
-		leaderboard += fmt.Sprintf(" %-6d|", val.TotalAttempts)
+		leaderboard += fmt.Sprintf(" %-8d|", val.TotalAttempts)
 		user, _ := ctx.Ses.User(val.UserId)
 		leaderboard += fmt.Sprintf(" %s#%s\n", user.Username, user.Discriminator)
 	}
