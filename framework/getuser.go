2171a30dfa7327eec11bfeb869356f9ac83a1145
Daren Liang <daren@darenliang.com>
2020-04-18 08:46:27 -0400

    Handle pfp direct messages

--- framework/getuser.go
+++ framework/getuser.go
@@ -3,39 +3,66 @@ package framework
 import (
 	"github.com/Necroforger/dgrouter/exrouter"
 	"github.com/bwmarrin/discordgo"
+	"strings"
 )
 
 func Getuser(ctx *exrouter.Context) *discordgo.User {
-
 	msg := ctx.Msg
 
-	if len(msg.Mentions) > 0 && msg.Mentions[0].ID != ctx.Ses.State.User.ID {
-		return msg.Mentions[0]
-	}
+	// Get full string after command invoke
+	user := strings.TrimSpace(ctx.Args.After(1))
 
-	if len(ctx.Args) < 2 {
+	// If no arguments
+	if len(user) == 0 {
 		return msg.Author
 	}
 
+	// Direct messages
+	if len(msg.GuildID) == 0 {
+		// Recipients don't include the bot itself
+		if matchUser(ctx.Ses.State.User, user) {
+			return ctx.Ses.State.User
+		}
+
+		dm, err := ctx.Ses.Channel(ctx.Msg.ChannelID)
+
+		if err != nil {
+			return nil
+		}
+
+		for _, u := range dm.Recipients {
+			if matchUser(u, user) {
+				return u
+			}
+		}
+
+		return nil
+	}
+
+	// Guild channels
 	g, err := ctx.Ses.Guild(msg.GuildID)
 
 	if err != nil {
 		return nil
 	}
 
-	user := ctx.Args[1]
-
 	for _, member := range g.Members {
 		u := member.User
-		if u.Username == user || u.String() == user || u.ID == user {
-			return member.User
+		if matchUser(u, user) {
+			return u
 		}
 	}
 
-	if len(ctx.Args) > 1 {
-		return nil
+	return nil
+}
+
+func matchUser(u *discordgo.User, uString string) bool {
+	if u.Username == uString || u.String() == uString || u.ID == uString || getMentionString(u) == uString {
+		return true
 	}
+	return false
+}
 
-	return msg.Author
-
+func getMentionString(u *discordgo.User) string {
+	return "<@!" + u.ID + ">"
 }
