729f1645ec994adaf7aea49a6265f3f993afe631
Daren Liang <daren@darenliang.com>
2020-04-19 19:07:23 -0400

    Clean helpers

--- framework/helper.go
+++ framework/helper.go
@@ -4,57 +4,11 @@ import (
 	"fmt"
 	"github.com/disintegration/imaging"
 	"image"
-	"io"
 	"math/rand"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 )
-
-// Index returns index of string found in list. Return -1 if not found
-func Index(str string, vs []string) int {
-	for i, val := range vs {
-		if val == str {
-			return i
-		}
-	}
-	return -1
-}
-
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) error {
-	// Get the data
-	resp, err := http.Get(url)
-
-	if err != nil {
-		return err
-	}
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-
-	err = resp.Body.Close()
-
-	if err != nil {
-		return err
-	}
-
-	err = out.Close()
-
-	if err != nil {
-		return err
-	}
-
-	return err
-}
 
 // RandomString returns random string at fixed length
 func RandomString(n int) string {
