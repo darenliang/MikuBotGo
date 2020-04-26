ee6b867157e6311423b6bf809fe50ff493941341
Daren Liang <daren@darenliang.com>
2020-04-26 19:35:34 -0400

    Dynamic rename

--- task.go
+++ task.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 // UpdatePresence updates the presence every 15 minutes
 func UpdatePresence() {
 	for {
-		_ = Session.UpdateStatus(0, "@MikuBot help")
+		_ = Session.UpdateStatus(0, fmt.Sprintf("@%s help", Session.State.User.Username))
 		time.Sleep(time.Minute * 15)
 	}
 }
