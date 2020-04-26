7e41934bc21cf3fa7ff13abdbd0fe45fdb1f2a4b
Daren Liang <daren@darenliang.com>
2020-04-26 19:33:38 -0400

    Rebrand as MikuBot

--- task.go
+++ task.go
@@ -7,7 +7,7 @@ import (
 // UpdatePresence updates the presence every 15 minutes
 func UpdatePresence() {
 	for {
-		_ = Session.UpdateStatus(0, "@MikuBotGo help")
+		_ = Session.UpdateStatus(0, "@MikuBot help")
 		time.Sleep(time.Minute * 15)
 	}
 }
