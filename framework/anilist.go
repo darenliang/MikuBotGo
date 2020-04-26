7a83d23882ef73b4fb0d6c50607b14a0f4202dae
Daren Liang <daren@darenliang.com>
2020-04-26 13:06:08 -0400

    Add new anilist response structs

--- framework/anilist.go
+++ framework/anilist.go
@@ -54,6 +54,58 @@ type AniListAnimeSearchResponse struct {
 	} `json:"data"`
 }
 
+type AniListMangaSearchResponse struct {
+	Data struct {
+		Media struct {
+			IDMal int `json:"idMal"`
+			Title struct {
+				Romaji        string `json:"romaji"`
+				English       string `json:"english"`
+				Native        string `json:"native"`
+				UserPreferred string `json:"userPreferred"`
+			} `json:"title"`
+			Type        string `json:"type"`
+			Format      string `json:"format"`
+			Status      string `json:"status"`
+			Description string `json:"description"`
+			StartDate   struct {
+				Year  int `json:"year"`
+				Month int `json:"month"`
+				Day   int `json:"day"`
+			} `json:"startDate"`
+			EndDate struct {
+				Year  int `json:"year"`
+				Month int `json:"month"`
+				Day   int `json:"day"`
+			} `json:"endDate"`
+			Volumes    int    `json:"volumes"`
+			Chapters   int    `json:"chapters"`
+			Source     string `json:"source"`
+			CoverImage struct {
+				ExtraLarge string `json:"extraLarge"`
+				Color      string `json:"color"`
+			} `json:"coverImage"`
+			Staff struct {
+				Edges []struct {
+					Node struct {
+						Name struct {
+							Full string `json:"full"`
+						} `json:"name"`
+					} `json:"node"`
+				} `json:"edges"`
+			} `json:"staff"`
+			Genres       []string `json:"genres"`
+			AverageScore int      `json:"averageScore"`
+			Rankings     []struct {
+				Rank    int    `json:"rank"`
+				Type    string `json:"type"`
+				AllTime bool   `json:"allTime"`
+			} `json:"rankings"`
+			SiteURL string `json:"siteUrl"`
+		} `json:"Media"`
+	} `json:"data"`
+}
+
 type TraceSearchResult struct {
 	Data struct {
 		Media struct {
@@ -140,5 +192,60 @@ func AnilistAnimeIDQuery(id int) map[string]string {
 }
 
 `, id)}
+	return jsonQuery
+}
+
+func AnilistMangaSearchQuery(query string) map[string]string {
+	jsonQuery := map[string]string{
+		"query": fmt.Sprintf(`
+{
+  Media(search: "%s", type: MANGA) {
+    idMal
+    title {
+      romaji
+      english
+      native
+      userPreferred
+    }
+    type
+    format
+    status
+    description
+    startDate {
+      year
+      month
+      day
+    }
+    endDate {
+      year
+      month
+      day
+    }
+    volumes
+    chapters
+    source
+    coverImage {
+      extraLarge
+      color
+    }
+    staff {
+      edges {
+        node {
+          name {
+            full
+          }
+        }
+      }
+    }
+    genres
+    averageScore
+    rankings {
+      rank
+      type
+      allTime
+    }
+    siteUrl
+  }
+}`, query)}
 	return jsonQuery
 }
