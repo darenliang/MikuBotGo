3722b3483e26bb6560bd70dff60f21b6021cc079
Daren Liang <daren@darenliang.com>
2020-05-12 19:53:35 -0400

    Add comments to command handler

--- handler.go
+++ handler.go
@@ -19,17 +19,20 @@ var Router = exrouter.New()
 // floppyEmoji
 const floppyEmoji = "\xf0\x9f\x92\xbe"
 
+// waitReady is a thread safe ready signal
 type waitReady struct {
 	ready bool
 	mux   sync.Mutex
 }
 
+// setReady sets a thread safe ready signal
 func (c *waitReady) setReady() {
 	c.mux.Lock()
 	c.ready = true
 	c.mux.Unlock()
 }
 
+// setReady gets a thread safe ready signal
 func (c *waitReady) getReady() bool {
 	c.mux.Lock()
 	defer c.mux.Unlock()
@@ -37,87 +40,67 @@ func (c *waitReady) getReady() bool {
 }
 
 func init() {
+	// Initialize ready signal
 	status := waitReady{
 		ready: false,
 	}
-	var joinGuilds = make(map[string]bool)
 
+	// Create map of existing guilds
+	var readyGuilds = make(map[string]bool)
+
+	// Add command handlers
+	// Utility
 	Router.OnMatch("info", dgrouter.NewRegexMatcher("^(?i)info$"), cmd.Info)
-
 	Router.OnMatch("prefix", dgrouter.NewRegexMatcher("^(?i)(prefix|p)$"), cmd.Prefix)
-
 	Router.OnMatch("pfp", dgrouter.NewRegexMatcher("^(?i)(pfp|avatar)$"), cmd.Pfp)
-
 	Router.OnMatch("cloc", dgrouter.NewRegexMatcher("^(?i)cloc$"), cmd.Cloc)
 
+	// Anime
 	Router.OnMatch("anime", dgrouter.NewRegexMatcher("^(?i)(anime|a)$"), cmd.Anime)
-
 	Router.OnMatch("manga", dgrouter.NewRegexMatcher("^(?i)(manga|m)$"), cmd.Manga)
-
 	Router.OnMatch("musicquiz", dgrouter.NewRegexMatcher("^(?i)(musicquiz|mq)$"), cmd.MusicQuiz)
-
 	Router.OnMatch("leaderboard", dgrouter.NewRegexMatcher("^(?i)(leaderboard|lb)$"), cmd.Leaderboard)
-
 	Router.OnMatch("trivia", dgrouter.NewRegexMatcher("^(?i)(trivia|t)$"), cmd.Trivia)
-
 	Router.OnMatch("waifu", dgrouter.NewRegexMatcher("^(?i)waifu$"), cmd.Waifu)
-
 	Router.OnMatch("sauce", dgrouter.NewRegexMatcher("^(?i)sauce$"), cmd.Sauce)
-
 	Router.OnMatch("identify", dgrouter.NewRegexMatcher("^(?i)identify$"), cmd.Identify)
 
+	// Fun
 	Router.OnMatch("gif", dgrouter.NewRegexMatcher("^(?i)gif$"), cmd.Gif)
-
 	Router.OnMatch("owofy", dgrouter.NewRegexMatcher("^(?i)owofy$"), cmd.Owofy)
-
 	Router.OnMatch("8ball", dgrouter.NewRegexMatcher("^(?i)(8ball|8b|eightball)$"), cmd.EightBall)
-
 	Router.OnMatch("kaomoji", dgrouter.NewRegexMatcher("^(?i)kaomoji$"), cmd.Kaomoji)
 
+	// Weeb
 	Router.OnMatch("catgirl", dgrouter.NewRegexMatcher("^(?i)catgirl$"), cmd.CatGirl)
-
 	Router.OnMatch("headpat", dgrouter.NewRegexMatcher("^(?i)(headpat|pat)$"), cmd.HeadPat)
-
 	Router.OnMatch("kiss", dgrouter.NewRegexMatcher("^(?i)kiss$"), cmd.Kiss)
-
 	Router.OnMatch("tickle", dgrouter.NewRegexMatcher("^(?i)tickle$"), cmd.Tickle)
-
 	Router.OnMatch("feed", dgrouter.NewRegexMatcher("^(?i)(feed|food|eat)$"), cmd.Feed)
-
 	Router.OnMatch("slap", dgrouter.NewRegexMatcher("^(?i)slap$"), cmd.Slap)
-
 	Router.OnMatch("cuddle", dgrouter.NewRegexMatcher("^(?i)cuddle$"), cmd.Cuddle)
-
 	Router.OnMatch("hug", dgrouter.NewRegexMatcher("^(?i)hug$"), cmd.Hug)
-
 	Router.OnMatch("smug", dgrouter.NewRegexMatcher("^(?i)smug$"), cmd.Smug)
-
 	Router.OnMatch("baka", dgrouter.NewRegexMatcher("^(?i)(baka|idiot)$"), cmd.Baka)
 
+	// Music
 	Router.OnMatch("add", dgrouter.NewRegexMatcher("^(?i)add$"), cmd.AddMusic)
-
 	Router.OnMatch("clear", dgrouter.NewRegexMatcher("^(?i)clear$"), cmd.ClearCommand)
-
 	Router.OnMatch("current", dgrouter.NewRegexMatcher("^(?i)current$"), cmd.CurrentCommand)
-
 	Router.OnMatch("join", dgrouter.NewRegexMatcher("^(?i)join$"), cmd.JoinCommand)
-
 	Router.OnMatch("leave", dgrouter.NewRegexMatcher("^(?i)(leave|disconnect)$"), cmd.LeaveCommand)
-
 	Router.OnMatch("pause", dgrouter.NewRegexMatcher("^(?i)pause$"), cmd.PauseCommand)
-
 	Router.OnMatch("play", dgrouter.NewRegexMatcher("^(?i)play$"), cmd.PlayCommand)
-
 	Router.OnMatch("queue", dgrouter.NewRegexMatcher("^(?i)queue$"), cmd.QueueCommand)
-
 	Router.OnMatch("shuffle", dgrouter.NewRegexMatcher("^(?i)shuffle$"), cmd.ShuffleCommand)
-
 	Router.OnMatch("skip", dgrouter.NewRegexMatcher("^(?i)skip$"), cmd.SkipCommand)
-
 	Router.OnMatch("stop", dgrouter.NewRegexMatcher("^(?i)stop$"), cmd.StopCommand)
 
+	// Help
 	Router.Default = Router.OnMatch("help", dgrouter.NewRegexMatcher("^(?i)(help|h)$"), func(ctx *exrouter.Context) {
 		msg := "Please visit __https://darenliang.github.io/MikuBot-Docs__ for help on all the commands."
+
+		// If DMs
 		if ctx.Msg.GuildID != "" {
 			msg = fmt.Sprintf("The current server prefix is %s\n", framework.PDB.GetPrefix(ctx.Msg.GuildID)) + msg
 		} else {
@@ -139,11 +122,13 @@ func init() {
 		// Load cache and check for new guilds
 		cache := framework.PDB.GetGuilds()
 		for _, guild := range ready.Guilds {
-			joinGuilds[guild.ID] = true
+			readyGuilds[guild.ID] = true
 			if cache[guild.ID] == "" {
 				framework.PDB.CreateGuild(guild.ID, config.Prefix)
 			}
 		}
+
+		// Set ready
 		status.setReady()
 	})
 
@@ -170,7 +155,7 @@ func init() {
 		_ = Router.FindAndExecute(Session, prefix, Session.State.User.ID, m.Message)
 	})
 
-	// Handle reaction add
+	// Handle reaction add for gif command
 	Session.AddHandler(func(_ *discordgo.Session, reaction *discordgo.MessageReactionAdd) {
 		// If DM
 		if reaction.GuildID == "" {
