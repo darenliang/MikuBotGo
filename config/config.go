4c71629990f5f06df1b6db063f64990bdeb3f1ea
Daren Liang <daren@darenliang.com>
2020-05-17 21:33:37 -0400

    Bump version

--- config/config.go
+++ config/config.go
@@ -2,9 +2,9 @@ package config
 
 // Includes config variables for bot
 const (
-	BotInfo       = "MikuBot v1.7.4"
-	EmbedColor    = 0x2e98a6
-	Prefix        = "!"
-	Timeout       = 60
-	Timer         = "\xe2\x8f\xb2\xef\xb8\x8f"
+	BotInfo    = "MikuBot v1.8.0"
+	EmbedColor = 0x2e98a6
+	Prefix     = "!"
+	Timeout    = 60
+	Timer      = "\xe2\x8f\xb2\xef\xb8\x8f"
 )
